caf09902d65febaa9850216ae15f7330371ab9b7
franchescap <86076982+franchescap@users.noreply.github.com>
2021-06-18 15:05:23 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,8 @@
+$(".to-pack-button").click(function(){
+    let pack=$(".to-pack-input").val();
+     $(".packing-list").append("<li>"+" im going to pack"+" "+pack+"</i>");
+    $(pack).css("color","purple");
+});
+$("h1").css("color","red");
+$("h1").css("color","red");
+$("h1").attr("src","http://cdn8.dissolve.com/p/D2137_4_595/D2137_4_595_0004_600.jpg");
